d78f5062758a3973409a32d92a703b47442ff021
minho-03 <pmh9038@naver.com>
2026-02-13 11:59:40 +0900

    미니트위터 만들기

---
+++ api/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request,jsonify
+
+app = (Flask)(__name__)
+app.users = {}
+app.id_count = 1 
+app.tweets = []
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "pong"
+
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    new_user = request.json                 # 회원정보 읽기
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count = app.id_count + 1
+
+    return jsonify(new_user)                # json형태로 전송 
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다.', 400
+
+    if len(tweet) > 300:
+        return '300자를 초과했습니다.' , 400
+    
+    user_id = int(payload['id'])
+
+    app.tweets.append({
+        'user_id' : user_id,
+        'tweet'   : tweet
+    })
+
+    return '', 200
+
+@app.route('/follow',methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int[payload['id']]
+    user_id_to_follow = int[payload['follow']]
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다.',400
+    
+    user = app.users[user_id]
+    user.setdefault('follow',set()).add(user_id_to_follow) #키가 존재하지 않으면 디폴트값을 지정하고, 만일 키가 이미 존재하면 해당 값을 읽어들이는 기능
+
+    return jsonify(user)
